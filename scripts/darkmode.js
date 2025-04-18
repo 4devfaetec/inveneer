20e91f0a9ac018c507523d0d70d4639c53036635
joaovitorfreitasmuniz <joaovitorfreitasmuniz@gmail.com>
2025-04-18 19:07:28 -0300

    dark mode toggle

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,11 +1,6 @@
 const themeToggler = document.querySelector(".theme-toggler");
 
-themeToggler.addEventListener("click", () => {
+function themeToggle() {
   document.body.classList.toggle("dark-mode");
-  themeToggler.querySelector("span:nth-child(1").classList.toggle("active");
-  themeToggler.querySelector("span:nth-child(2").classList.toggle("active");
-});
-
-document.body.classList.toggle("dark-mode");
-
-// alert("Funciona")
+  
+}
